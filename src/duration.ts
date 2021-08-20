8ce459862d4528f16a13bbcdb6ba8ceb8d7df732
Alexander Håkansson <alex.hsson@gmail.com>
2021-08-20 13:02:37 +0200

    Adds TSDoc comments

--- src/duration.ts
+++ src/duration.ts
@@ -15,66 +15,153 @@ const UNIT_HOUR = 'h';
 export class Duration {
     private constructor(private readonly value: number) {}
 
+    /**
+     * Create a new Duration instance representing d nanoseconds
+     *
+     * @param d - nanoseconds
+     * @returns a Duration representing d nanoseconds
+     */
     static nanosecond(d: number): Duration {
         return new Duration(d * NANOSECOND);
     }
 
+    /**
+     * Create a new Duration instance representing d microseconds
+     *
+     * @param d - microseconds
+     * @returns a Duration representing d microseconds
+     */
     static microsecond(d: number): Duration {
         return new Duration(d * MICROSECOND);
     }
 
+    /**
+     * Create a new Duration instance representing d milliseconds
+     *
+     * @param d - milliseconds
+     * @returns a Duration representing d milliseconds
+     */
     static millisecond(d: number): Duration {
         return new Duration(d * MILLISECOND);
     }
 
+    /**
+     * Create a new Duration instance representing d seconds
+     *
+     * @param d - seconds
+     * @returns a Duration representing d seconds
+     */
     static second(d: number): Duration {
         return new Duration(d * SECOND);
     }
 
+    /**
+     * Create a new Duration instance representing d minutes
+     *
+     * @param d - minutes
+     * @returns a Duration representing d minutes
+     */
     static minute(d: number): Duration {
         return new Duration(d * MINUTE);
     }
 
+    /**
+     * Create a new Duration instance representing d hours
+     *
+     * @param d - hours
+     * @returns a Duration representing d hours
+     */
     static hour(d: number): Duration {
         return new Duration(d * HOUR);
     }
 
+    /**
+     * Calculate the duration until the Date t
+     *
+     * @param t - a Date
+     * @returns The Duration until t
+     */
     static until(t: Date): Duration {
         const now = this.millisecond(new Date().getTime());
         const then = this.millisecond(t.getTime());
         return then.sub(now);
     }
 
+    /**
+     * Calculate the duration since the Date t
+     *
+     * @param t - a Date
+     * @returns The Duration since t
+     */
     static since(t: Date): Duration {
         const now = this.millisecond(new Date().getTime());
         const then = this.millisecond(t.getTime());
         return now.sub(then);
     }
 
+    /**
+     * Get the duration as nanoseconds
+     *
+     * @returns duration in nanoseconds
+     */
     public get nanoseconds(): number {
         return this.value;
     }
 
+    /**
+     * Get the duration as microseconds
+     *
+     * @returns duration in microseconds
+     */
     public get microseconds(): number {
         return this.value / MICROSECOND;
     }
 
+    /**
+     * Get the duration as milliseconds
+     *
+     * @returns duration in milliseconds
+     */
     public get milliseconds(): number {
         return this.value / MILLISECOND;
     }
 
+    /**
+     * Get the duration as seconds
+     *
+     * @returns duration in seconds
+     */
     public get seconds(): number {
         return this.value / SECOND;
     }
 
+    /**
+     * Get the duration as minutes
+     *
+     * @returns duration in minutes
+     */
     public get minutes(): number {
         return this.value / MINUTE;
     }
 
+    /**
+     * Get the duration as hours
+     *
+     * @returns duration in hours
+     */
     public get hours(): number {
         return this.value / HOUR;
     }
 
+    /**
+     * Return a string representing the duration in the form "72h3m0.5s".
+     * Leading zero units are omitted. As a special case, durations less than
+     * one second format use a smaller unit (milli-, micro-, or nanoseconds)
+     * to ensure that the leading digit is non-zero. The zero duration
+     * formats as 0s.
+     *
+     * @returns a string representing the duration
+     */
     public toString(): string {
         const abs = Math.abs(this.value);
         const prefix = this.value < 0 ? '-' : '';
@@ -101,14 +188,33 @@ export class Duration {
         }
     }
 
+    /**
+     * Addition with the specified duration: this + d
+     *
+     * @param d - the duration to add to the current
+     * @returns the sum this + d
+     */
     add(d: Duration): Duration {
         return new Duration(this.value + d.value);
     }
 
+    /**
+     * Subtraction of the specified duration: this - d
+     *
+     * @param d - the duration to remove from the current
+     * @returns the difference this - d
+     */
     sub(d: Duration): Duration {
         return new Duration(this.value - d.value);
     }
 
+    /**
+     * Rounds the duration to the nearest multiple of the specified duration.
+     * If m is zero or negative, the duration is unchanged.
+     *
+     * @param m - the duration multiple to round to
+     * @returns this duration rounded to the nearest multiple of m
+     */
     round(m: Duration): Duration {
         if (m.value <= 0) {
             return this;
